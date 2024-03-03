38d297b9b9171798713339b1f79529286c41f343
Mohammed Salem <mohammedsalem@Mohammeds-MacBook-Pro.local>
2024-03-03 15:55:34 +0300

    git init

---
+++ lib-a/src/main/java/com/example/lib_a/LibAClass.java
@@ -0,0 +1,10 @@
+package com.example.lib_a;
+
+public class LibAClass {
+    public int fun0(){
+        return 10;
+    }
+    public static void main(String[] args) {
+        System.out.println("LibAClass.");
+    }
+}
