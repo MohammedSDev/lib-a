9219725e7c5d6dce5f60f7b2aa8b72fbeeabbc3b
Mohammed Salem <mohammedsalem@Mohammeds-MacBook-Pro.local>
2024-03-03 15:28:52 +0300

    git init

---
+++ lilb-a/src/main/java/com/example/lilb_a/LibAClass.java
@@ -0,0 +1,10 @@
+package com.example.lilb_a;
+
+public class LibAClass {
+    public int fun0(){
+        return 10;
+    }
+    public static void fun1() {
+        System.out.println("LibAClass.");
+    }
+}
